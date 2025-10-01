30ee9cdeab80f8af4e1f3776376bc7b63794e974
cryptowebs <admin@cryptowebs.io>
2025-10-01 00:19:33 -0400

    Initial Commit

---
+++ BadHabitBreaker/BadHabitBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadHabitBreakerApp.swift
+//  BadHabitBreaker
+//
+//  Created by cw on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BadHabitBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BadHabitBreakerTests/BadHabitBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BadHabitBreakerTests.swift
+//  BadHabitBreakerTests
+//
+//  Created by cw on 10/1/25.
+//
+
+import Testing
+@testable import BadHabitBreaker
+
+struct BadHabitBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
